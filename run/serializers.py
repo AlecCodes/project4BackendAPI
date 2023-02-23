ef0937645c1dbc85ec7e9792bd3f928e55fb52fd
Alec Hannaford <alec322@gmail.com>
2023-02-23 15:27:53 -0800

    serializer

---
+++ run/serializers.py
@@ -0,0 +1,8 @@
+from .models import Run
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+class RunSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Run
+        fields = ['id','subject','details']
