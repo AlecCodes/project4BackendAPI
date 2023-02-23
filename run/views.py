92d3d53976eb01cfb12fb649a79d2756840a4526
Alec Hannaford <alec322@gmail.com>
2023-02-23 15:32:30 -0800

    added viewset

--- run/views.py
+++ run/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Run
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import RunSerializer
 
 # Create your views here.
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+    permission_classes = [permissions.AllowAny]
