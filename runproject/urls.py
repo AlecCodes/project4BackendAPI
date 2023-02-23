e1e79f3aaa2b8545c932e4c431b2104428816b0c
Alec Hannaford <alec322@gmail.com>
2023-02-23 15:33:56 -0800

    router

--- runproject/urls.py
+++ runproject/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from run.views import RunViewSet
+
+router = routers.DefaultRouter()
+router.register(r'run', RunViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
