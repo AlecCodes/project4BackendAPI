c74c85a240c3f170be0e4fb7cf6d3416ddfd85ff
Alec Hannaford <alec322@gmail.com>
2023-02-23 18:55:14 -0800

    changed model

--- run/models.py
+++ run/models.py
@@ -5,3 +5,4 @@ class Run(models.Model):
     name = models.CharField(max_length=100)
     category = models.CharField(max_length=100)
     dur = models.IntegerField(default=0)
+    distance = models.IntegerField(default = None, blank = True, null = True)
--- run/serializers.py
+++ run/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class RunSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Run
-        fields = ['id','name','category']
+        fields = ['id','name','category','dur','distance']
